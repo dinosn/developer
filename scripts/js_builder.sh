08d703643280c00efc91caf09911c2be51c3cad7
kristof de spiegeleer <despiegk@kristofs-iMac.local>
2017-04-10 11:50:53 +0200

    builder

--- scripts/js_builder.sh
+++ scripts/js_builder.sh
@@ -17,5 +17,9 @@ sleep 1
 
 ssh root@localhost -p 2222 'apk update;apk add curl;apk add mc'
 
+# #capnp needs to be installed before doing jumpscale
+# scp -P 2222 helpers/capnp.sh root@localhost:/tmp/
+# ssh -A root@localhost -p 2222 'cd /tmp;sh capnp.sh'
+
 #important to use local keys
 ssh -A root@localhost -p 2222 'cd $TMPDIR;rm -f install.sh;export JSBRANCH="8.2.0";curl -k https://raw.githubusercontent.com/Jumpscale/jumpscale_core8/$JSBRANCH/install/install.sh?$RANDOM > install.sh; bash install.sh'
