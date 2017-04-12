849a3bf48054ba998c0b5d2966049b26941c05de
kristof de spiegeleer <kristof@greenitglobe.com>
2017-04-12 06:54:04 +0200

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,5 +1,6 @@
 
 #this is the main env file which needs to be sourced for any action we do on our platform
+set -ex
 
 if [ "$(uname)" == "Darwin" ]; then
     export LANG=C; export LC_ALL=C
@@ -27,16 +28,47 @@ if [ -z "$SSH_AUTH_SOCK" ] ; then
 fi
 
 
-if [ -z "$SSHKEYNAME" ]; then
-    if [ -e $HOMEDIR/.ssh/id_rsa.pub ]; then
-        export SSHKEYNAME='id_rsa'
-    else
-        echo "Need to set SSHKEYNAME (through export SSHKEYNAME='mykey')"
-        exit 1
-    fi
+if [ -z $SSHKEYNAME]; then
+    SSHKEYNAME=SOMETHINGWHICHWILLNOTMATCH
+else
+    #trim the sshkeyname
+    export SSHKEYNAME=`echo $SSHKEYNAME | xargs`
 fi
 
-ssh-add $HOMEDIR/.ssh/$SSHKEYNAME
+while [ ! -e "$HOMEDIR/.ssh/$SSHKEYNAME" ]
+do
+    echo "please give name of ssh key to load, if not generate yet do so."
+    read -p 'SSHKEYNAME: ' SSHKEYNAME
+    echo "check keypath '$HOMEDIR/.ssh/$SSHKEYNAME' exists"
+
+done
+
+set +e
+keyexists=$(ssh-add -l| grep $SSHKEYNAME)
+#check exit code if not 0 then means key did not exist
+if [ $? -eq 0 ]; then
+    set -e
+    echo "KEY $SSHKEYNAME was loaded"
+else
+    set -e
+    echo "will now try to load sshkey: $HOMEDIR/.ssh/$SSHKEYNAME"
+    ssh-add $HOMEDIR/.ssh/$SSHKEYNAME
+    echo "KEY loaded"
+fi
+
+
+#check profile file exists, if yes modify
+if [ -e ~/.profile ] ; then
+    sed  '/export SSHKEYNAME/d'  ~/.profile > ~/.profile2;mv ~/.profile2 ~/.profile
+    echo export SSHKEYNAME=$SSHKEYNAME >> ~/.profile
+fi
+
+
+
+#now add to profile
+
+
+
 
 export DATADIR=$HOMEDIR/data
 export CODEDIR=$HOMEDIR/code
