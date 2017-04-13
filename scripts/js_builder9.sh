b0ba5795303efd5a3d6a5bf1eabb5cce5c06d026
kristof de spiegeleer <kristof@incubaid.com>
2017-04-13 08:41:10 +0200

    init

--- scripts/js_builder9.sh
+++ scripts/js_builder9.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-docker rm --force js8
+docker rm --force js9
 mkdir -p /optvar/data
 
 set -ex
@@ -9,15 +9,15 @@ if [ -z "$SSHKEYNAME" ]; then
     exit 1
 fi
 
-rm -rf /Users/despiegk/.ssh/known_hosts
+rm -rf ~/.ssh/known_hosts
 
 docker run --name js9 -h js9 -d -p 2223:22 -v ~/.ssh/$SSHKEYNAME.pub:/root/.ssh/authorized_keys -v $CODEDIR:/opt/code/ -v $DATADIR:/data macropin/sshd
 
 sleep 1
 
-ssh root@localhost -p 2222 'apk update;apk add curl;apk add mc'
+ssh root@localhost -p 2223 'apk update;apk add curl;apk add mc'
 
 #important to use local keys
-ssh -A root@localhost -p 2222 'cd $TMPDIR;rm -f install.sh;export JSBRANCH="8.2.0";curl -k https://raw.githubusercontent.com/Jumpscale/jumpscale_core9/$JSBRANCH/install/install.sh?$RANDOM > install.sh; bash install.sh'
+ssh -A root@localhost -p 2223 'cd $TMPDIR;rm -f install.sh;export JSBRANCH="master";curl -k https://raw.githubusercontent.com/Jumpscale/jumpscale_core9/$JSBRANCH/install/install.sh?$RANDOM > install.sh; bash install.sh'
 
-ssh -A root@localhost -p 2222 'echo "JS9 DEVELOPER WELCOME\n" > /etc/motd'
+ssh -A root@localhost -p 2223 'echo "JS9 DEVELOPER WELCOME\n" > /etc/motd'
