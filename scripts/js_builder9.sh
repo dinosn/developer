fcca3c3663df3267fd0cafe0abf86a55f7c414f1
kristof de spiegeleer <kristof@incubaid.com>
2017-04-17 16:37:04 +0400

    Tmux

--- scripts/js_builder9.sh
+++ scripts/js_builder9.sh
@@ -15,7 +15,7 @@ docker run --name js9 -h js9 -d -p 2223:22 -v ~/.ssh/$SSHKEYNAME.pub:/root/.ssh/
 
 sleep 1
 
-ssh root@localhost -p 2223 'apk update;apk add curl;apk add mc;apk add python3'
+ssh root@localhost -p 2223 'apk update;apk add curl;apk add mc;apk add python3;apk add tmux'
 ssh root@localhost -p 2223 'cd $TMPDIR;rm -rf get-pip.py;curl -k https://bootstrap.pypa.io/get-pip.py > get-pip.py;python3 get-pip.py'
 ssh root@localhost -p 2223 'pip3 install --upgrade pip'
 ssh root@localhost -p 2223 'pip3 install -e /opt/code/github/jumpscale/core9 --upgrade'
