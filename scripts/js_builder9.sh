f256c52a2d5db0cde5f3a18b5d3c13f27c3dfe45
kristof de spiegeleer <kristof@incubaid.com>
2017-04-27 14:02:48 +0400

    qqq

--- scripts/js_builder9.sh
+++ scripts/js_builder9.sh
@@ -149,7 +149,7 @@ valid
 
 echo "* first init of jumpscale 9"
 docker exec -t js9 /bin/sh -c 'python3 -c "from js9 import j;j.do.initEnv()"' > /tmp/lastcommandoutput.txt 2>&1 #need to do it twice, ignore error first time
-docker exec -t js9 /bin/sh -c 'from js9 import j;j.tools.jsloader.generate()"' > /tmp/lastcommandoutput.txt 2>&1 #need to do it twice, ignore error first time
+docker exec -t js9 /bin/sh -c 'python3 -c "from JumpScale9 import j;j.tools.jsloader.generate()"' > /tmp/lastcommandoutput.txt 2>&1 #need to do it twice, ignore error first time
 docker exec -t js9 /bin/sh -c 'python3 -c "from js9 import j"' > /tmp/lastcommandoutput.txt 2>&1
 valid
 
