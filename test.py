8a97caeb55fd61ac426a3ba40a02bad487c78b31
root <root@js9>
2017-05-02 05:49:51 +0000

    cuisine rework

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 from js9 import j
 
-path = "/opt/code/github/jumpscale"
+# path = "/opt/code/github/jumpscale"
+#
+# for item in j.sal.fs.listFilesInDir(path, True):
+#     if "cuisine" in item.lower():
+#         newname = item.replace("cuisine", "prefab")
+#         newname = newname.replace("Prefab", "Prefab")
+#         j.sal.fs.renameFile(item, newname)
+#         print("rename %s %s" % (item, newname))
 
-for item in j.sal.fs.listFilesInDir(path, True):
-    if "cuisine" in item.lower():
-        newname = item.replace("cuisine", "prefab")
-        newname = newname.replace("Prefab", "Prefab")
-        j.sal.fs.renameFile(item, newname)
-        print("rename %s %s" % (item, newname))
+from IPython import embed
+print("DEBUG NOW test")
+embed()
+raise RuntimeError("stop debug here")
