188ac20e6a58a8fce6436f1540da6bfa66722f76
Ashraf Fouda <ashraf.m.fouda@gmail.com>
2017-05-22 18:44:10 +0200

    clear should not used here it will ruin any prefab.core.run with profile=True
    Signed-off-by: Ashraf Fouda <ashraf.m.fouda@gmail.com>

--- files_guest/root/.jsenv.sh
+++ files_guest/root/.jsenv.sh
@@ -1,11 +1,10 @@
 
-clear
 
 export CODEDIR=/opt/code
 export GIGDIR=/root/gig
 mkdir -p $GIGDIR
 
+export TERM=screen-256color
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
-export TERM=screen-256color
 export PS1="gig:\h:\w$\[$(tput sgr0)\]"
