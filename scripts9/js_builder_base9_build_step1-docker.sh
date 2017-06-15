a83a9b0451ef5fa8d588e3494f79c5ceb3819e9d
despiegk <kristof@incubaid.com>
2017-06-15 12:15:00 +0200

    std build is now a small docker, use -l to have all libs inside

--- scripts9/js_builder_base9_build_step1-docker.sh
+++ scripts9/js_builder_base9_build_step1-docker.sh
@@ -29,19 +29,20 @@ echo "[+]   installing python"
 apt-get install -y python3 > ${logfile} 2>&1
 
 echo "[+]   installing basic dependencies"
-apt-get install -y curl mc openssh-server git net-tools iproute2 tmux localehelper psmisc > ${logfile} 2>&1
+apt-get install -y curl mc openssh-server git net-tools iproute2 tmux localehelper psmisc python3-cryptography python3-psutil> ${logfile} 2>&1
 
 echo "[+]   setting up default environment"
 echo "" > /etc/motd
 touch /root/.iscontainer
 
-echo "[+]   authorizing users"
-mkdir -p /root/.ssh
-chmod 700 /root/.ssh
-for user in $(curl -s https://raw.githubusercontent.com/Jumpscale/developer/master/scripts/devs); do
-    echo "[+]      authorizing $user" > ${logfile}
-    curl -s https://github.com/${user}.keys >> /root/.ssh/authorized_keys
-done
+#REALLY BAD PRACTICE DO NOT DO THIS
+# echo "[+]   authorizing users"
+# mkdir -p /root/.ssh
+# chmod 700 /root/.ssh
+# for user in $(curl -s https://raw.githubusercontent.com/Jumpscale/developer/master/scripts/devs); do
+#     echo "[+]      authorizing $user" > ${logfile}
+#     curl -s https://github.com/${user}.keys >> /root/.ssh/authorized_keys
+# done
 
 echo "[+]   installing pip system"
 cd /tmp
@@ -52,6 +53,9 @@ pip3 install --upgrade pip > ${logfile} 2>&1
 pip3 install tmuxp > ${logfile} 2>&1
 pip3 install gitpython > ${logfile} 2>&1
 
+echo "[+]   syncronizing developer files"
+rsync -rv /opt/code/github/jumpscale/developer/files_guest/ / > ${logfile} 2>&1
+
 echo "[+]   installing jumpscale core9"
 pip3 install -e /opt/code/github/jumpscale/core9 --upgrade > ${logfile} 2>&1
 
@@ -59,8 +63,7 @@ echo "[+]   installing jumpscale prefab9"
 pip3 install -e /opt/code/github/jumpscale/prefab9 --upgrade > ${logfile} 2>&1
 
 
-echo "[+]   syncronizing developer files"
-rsync -rv /opt/code/github/jumpscale/developer/files_guest/ / > ${logfile} 2>&1
+
 
 echo "[+]   installing binaries files"
 # source /root/.jsenv.sh
